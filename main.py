02a5512251ab8706f74f255d0c66a6300412d764
KSG <ksg.dev.data@gmail.com>
2024-05-10 16:09:12 -0500

    money and report functions

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import menu
 
 
-def report():
+def report(balance):
+    uom = ["ml", "ml", "g"]
     for resource, amount in menu.resources.items():
         print(f"{resource.title()}: {amount}")
+
+    print(f"Money: ${balance:.2f}")
 
 
 def order():
@@ -17,6 +20,15 @@ def check_resources():
 def money():
     print("Please insert coins.")
     q = int(input("How many quarters?: "))
+    d = int(input("How many dimes?: "))
+    n = int(input("How many nickels?: "))
+    p = int(input("How many pennies?: "))
+    q_total = 0.25 * q
+    d_total = 0.10 * d
+    n_total = 0.05 * n
+    p_total = 0.01 * p
+    total = sum([q_total, d_total, n_total, p_total])
+    return float(total)
 
 
 # operations = {
@@ -26,9 +38,14 @@ def money():
 
 
 def main():
+    entered = 0
     choice = input("What would you like? (espresso/latte/cappuccino): ").lower().strip()
     if choice == "report":
-        report()
+        report(entered)
+    # Test money function, return report
+    elif choice == "money":
+        entered += money()
+        report(entered)
 
 
 main()
