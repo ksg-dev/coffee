825e972dc921acbb55a26f5c722d2b0401091f14
ksg-dev <ksg.dev.data@gmail.com>
2024-05-14 12:14:58 -0500

    final coffee maker

--- OOP Rework/main.py
+++ OOP Rework/main.py
@@ -1,3 +1,28 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+coffeemaker = CoffeeMaker()
+atm = MoneyMachine()
+
+
+
+def main():
+    maker_on = True
+    while maker_on:
+        choice = input("What would you like? (espresso/latte/cappuccino): ")
+        if choice == "off":
+            maker_on = False
+            print("Have a nice day!")
+        elif choice == "report":
+            coffeemaker.report()
+            atm.report()
+        elif choice == "espresso" or "latte" or "cappuccino":
+            drink = Menu().find_drink(choice)
+            if coffeemaker.is_resource_sufficient(drink):
+                if atm.make_payment(drink.cost):
+                    coffeemaker.make_coffee(drink)
+            
+
+
+main()
